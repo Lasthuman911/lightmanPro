bbefe06937b006baf2e1c8c3ba32d0949e26bd63
Administrator <lightman110@163.com>
2018-08-11 23:19:33 +0800

    mybatis 格式

--- springboot/mybatis_generator/src/main/java/lightman/mybatis_generator/MybatisGeneratorApplication.java
+++ springboot/mybatis_generator/src/main/java/lightman/mybatis_generator/MybatisGeneratorApplication.java
@@ -11,9 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("lightman.mybatis_generator.dao")
 public class MybatisGeneratorApplication {
 
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args){
         SpringApplication.run(MybatisGeneratorApplication.class, args);
     }
 }
